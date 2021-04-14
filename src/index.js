9524dd055591f5f60d167af1475a1419653227d5
ChaonengTan <chaonengtan@gmail.com>
2021-04-14 16:26:18 -0700

    +jestTest, +100%coverage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+function padWithZero(num){
+    if (num<10){
+        return`0${num}`
+    }
+    return num
+}
 class D {
     constructor(...args) {
         this._date = new Date(...args)
     }
+    // getters
     get year() {
         return this._date.getFullYear()
     }
@@ -10,56 +17,45 @@ class D {
     }
     get month() {
         const months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        return months[this._date.getMonth()]
+        return months[this._date.getMonth()-1]
     }
     get mon() {
         const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-        return months[this._date.getMonth()]
+        return months[this._date.getMonth()-1]
     }
     get week() {
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday', 'Saturday']
-        return days[this._date.getDay()]
+        const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        return days[this._date.getDay()-3]
     }
     get wk() {
-        const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-        return String(days[this._date.getDay()]).slice(0, 4)
+        const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        return String(days[this._date.getDay()-3]).slice(0, 4)
     }
     get dates() {
-        if (this._date.getDate()<9){
-            return(0,+this._date.getDate())
-        }
-        return this._date.getDate()
+        return padWithZero(this._date.getDate())
     }
     get date() {
         return this._date.getDate()
     }
     get hours() {
-        if (this._date.getHours()<9){
-            return(0,+this._date.getHours())
-        }
-        return this._date.getHours()
+        return padWithZero(this._date.getHours())
     }
     get hr() {
         return this._date.getHours()
     }
     get minutes() {
-        if (this._date.getMinutes()<9){
-            return(0,+this._date.getMinutes())
-        }
-        return this._date.getMinutes()
+        return padWithZero(this._date.getMinutes())
     }
     get mins() {
         return this._date.getMinutes()
     }
     get seconds() {
-        if (this._date.getSeconds()<9){
-            return(0,+this._date.getSeconds())
-        }
-        return this._date.getSeconds()
+        return padWithZero(this._date.getSeconds())
     }
     get secs() {
         return this._date.getSeconds()
     }
+    // functionals
     format(format) {
         const formatChar = {
             'Y':this.year, 
@@ -87,8 +83,8 @@ class D {
     }
     when(){
         const rn = new Date()
-        console.log('input: ', this.year, this._date.getMonth(), this._date.getDay())
-        console.log('right now: ', rn.getFullYear(), rn.getMonth(), rn.getDay())
+        // console.log('input: ', this.year, this._date.getMonth(), this._date.getDay())
+        // console.log('vs: ',rn.getFullYear(), rn.getMonth(), rn.getDay())
         if (this.year-rn.getFullYear()!=0){
             return (this.year-rn.getFullYear()+ 'years')
         }
@@ -98,18 +94,6 @@ class D {
         return (this._date.getDay()-rn.getDay()+ 'days')
     }
 }
-module.exports = D
-// const rn = new D()
-// const someday = new D(2200, 7, 17)
-// console.log(rn.year)
-// console.log(rn.yr)
-// console.log(rn.month)
-// console.log(rn.mon)
-// console.log(rn.week)
-// console.log(rn.wk)
-// console.log(rn.date)
-// console.log(rn.hours)
-// console.log(rn.mins)
-// console.log(rn.secs)
-// console.log(rn.format('Y/m/D/h/I/s'))
-// console.log(someday.when())
+module.exports = {
+    D: D
+}
